d90919f0441adb9707462cfb2dd8bfd877714c73
Anders Lybecker <anders@lybecker.com>
2016-07-30 01:59:27 +0200

    added search client stub

---
+++ GetMeAGuru/Engagements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GetMeAGuru
+{
+    public class Engagements
+    {
+        public string Id { get; set; }
+        public string Alias { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public string Company { get; set; }
+        public DateTime Date { get; set; }
+        public string[] Tech { get; set; }
+        public string[] Resources { get; set; }
+    }
+}
